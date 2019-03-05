233bb6e751f6ccc3388651b36405cd705f1c1046
JacopoWolf <jacopo178.c@gmail.com>
2019-03-05 13:01:58 +0100

    Updated all classes
    * Added test project
    
    * added reference to JDS
    
    * update of Generics in interfaces and Tree
    
    1- updated test project
    2- updated IComposite fixing some problems with the Generic typing
    3- implemented getAllNodes() in the Tree class
    
    * implemented getAllRecursive in IComposite

--- JavaDataStructures/src/JDS/patterns/composite/IComposite.java
+++ JavaDataStructures/src/JDS/patterns/composite/IComposite.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 import java.util.function.Predicate;
 
 /**
- *
+ * basic interface for Composite elements
  * @author Jacopo_Wolf
  * @param <T>
  */
@@ -19,4 +19,22 @@ public interface IComposite<T> extends IElement<T>
     public boolean containsRecursive( Predicate<IElement<T>> predicate );
     
     public Collection< ? extends IElement<T> > findRecursive(Predicate<IElement<T>> predicate );
+   
+    /**
+     * recursively returns all sub elements of root
+     * @param <A> the content of the elements
+     * @param root the root element
+     * @param out the Collection to wich add the elements
+     */
+    public static <A> void getAllRecursive( IComposite<A> root, Collection<IElement<A>> out )
+    {
+        for ( IElement<A> el : root.getSubElements() )
+        {
+            out.add(el);
+            
+            if ( el instanceof IComposite )
+                IComposite.getAllRecursive( (IComposite<A>)el , out);
+        }
+    }
+
 }

549536bca649580bc094472d3ff78035e75fbbd9
JacopoWolf <work.jacopocomparin@gmail.com>
2019-03-24 16:39:57 +0100

    Arch complete documentation

--- JavaDataStructures/src/JDS/structures/graphs/Arch.java
+++ JavaDataStructures/src/JDS/structures/graphs/Arch.java
@@ -4,22 +4,40 @@
 package JDS.structures.graphs;
 
 
+/**
+ * implements IArch, rappresents uniateral connection between two nodes.
+ * @author Jacopo_Wolf
+ * @param <T> The type of content 
+ */
 public class Arch<T> implements IArch<T>
 {
     private T data;
             IGraphNode pointsto;
     
+            
+    /**
+     * 
+     * @return the data stored in this arch
+     */
     @Override
     public T getData()
     {
         return this.data;
     }
+    /**
+     * sets the data contained in this arch
+     * @param data new data
+     */
     public void setData( T data)
     {
         this.data = data;
     }
 
     
+    /**
+     * 
+     * @return the IGraphNode this Arch points to
+     */
     @Override
     public IGraphNode pointsTo()
     {
@@ -27,18 +45,28 @@ public class Arch<T> implements IArch<T>
     }
     
     
-
+    /**
+     * initializes a new Arch
+     * @param data set the data of the Arch
+     * @param pointsto the node this Arch will point to
+     */
     public Arch( T data, IGraphNode pointsto )
     {
         this.data = data;
         this.pointsto = pointsto;
     }
-
+    /**
+     * initializes a new Arch
+     * @param pointsto the node this arch will point to
+     */
     public Arch( IGraphNode pointsto )
     {
         this.data = null;
         this.pointsto = pointsto;
     }
+    /**
+     * initializes a new Arch
+     */
     public Arch()
     {
         this.data = null;
